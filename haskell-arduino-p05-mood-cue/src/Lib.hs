c1ce5bc0d9cc41754c931a86a3b37cc52b17c618
McKay Broderick <mckay.db@gmail.com>
2020-12-16 21:17:33 -0700

    Seemed to get better coordinates. But I'm not sure why yet

--- haskell-arduino-p05-mood-cue/src/Lib.hs
+++ haskell-arduino-p05-mood-cue/src/Lib.hs
@@ -8,13 +8,15 @@ import qualified Copilot.Arduino.Library.Serial as Serial
 
 someFunc :: IO ()
 someFunc = arduino $ do
-    potVal <- unsafeCast <$> (input a0 :: Sketch (Behavior ADC)) :: Sketch (Behavior Float)
+    potVal <- cast <$> (input a0 :: Sketch (Behavior ADC)) :: Sketch (Behavior Int32)
 
     Serial.baud 9600
     Serial.device =: [ Serial.str "potVal: "
                      , Serial.show potVal
                      ]
-    let angle = Servo.transform potVal
+    -- TODO: Figure out why do I need to map this again to values that are close to 60 and 245?
+    let angle = Servo.intTransform' 0 1023 60 245 potVal
+
     Serial.device =: [ Serial.str "angle: "
                      , Serial.show angle
                      , Serial.char '\n'
